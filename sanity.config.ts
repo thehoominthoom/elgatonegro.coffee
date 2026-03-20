b546a84caa886492ee431f33cb9179359b95ad62
thehoominthoom <phil@thirdlabel.us>
2026-03-19 22:04:10 -0500

    fix: update Sanity Studio basePath to /admin/studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -14,7 +14,7 @@ import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
 
 export default defineConfig({
-  basePath: '/studio',
+  basePath: '/admin/studio',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
